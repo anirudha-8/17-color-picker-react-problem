2c1ae2e6e6f979d25160b6298a759395381462bf
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-12 08:00:18 +0530

    add color input to select color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import "./App.css";
 function App() {
 	const [selectedColor, setSelectedColor] = useState("#000000");
 
-	return <div></div>;
+	return (
+		<div>
+			<input
+				type="color"
+				name="color"
+				id="color"
+				onChange={(e) => setSelectedColor(e.target.value)}
+				value={selectedColor}
+			/>
+		</div>
+	);
 }
 
 export default App;
