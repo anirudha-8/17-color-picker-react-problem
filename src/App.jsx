c28f8493a0e52575ccb7917c39f611e018661fde
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-12 08:09:23 +0530

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,28 @@ function App() {
 
 	return (
 		<div>
-			<label htmlFor="color">Select Color: </label>
-			<input
-				type="color"
-				name="color"
-				id="color"
-				onChange={(e) => setSelectedColor(e.target.value)}
-				value={selectedColor}
-			/>
+			<div>
+				<label htmlFor="color">Select Color: </label>
+				<input
+					type="color"
+					name="color"
+					id="color"
+					onChange={(e) => setSelectedColor(e.target.value)}
+					value={selectedColor}
+				/>
+			</div>
 			<hr />
-			<h1>Preview of selected color...</h1>
-			<div
-				style={{
-					width: "200px",
-					height: "200px",
-					border: "1px solid gray",
-					backgroundColor: selectedColor,
-				}}
-			></div>
+			<div>
+				<h1>Preview of selected color...</h1>
+				<div
+					style={{
+						width: "200px",
+						height: "200px",
+						border: "1px solid gray",
+						backgroundColor: selectedColor,
+					}}
+				></div>
+			</div>
 		</div>
 	);
 }
